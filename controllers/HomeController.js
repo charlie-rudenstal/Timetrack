40c982a451f339682bb70312e6b4458b8afb685a
charlie-rudenstal <charlie4@gmail.com>
2012-08-16 23:56:16 +0200

    Better member names

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -4,7 +4,7 @@ module.exports = {
 	get_index : function(req, res) {
 		
 		var member = new models.Member();
-		member.name = 'woop';
+		member.name = Math.random() >= 0.5 ? 'Charlie' : 'Julian';
 		member.save();
 
 		models.Member.find({}, function(err, members) {
